ac418a9c19343976d1f4e437ed1621fa73555ccc
Benjamin Joseph <benjo@sayhello.com>
2016-04-07 16:40:57 -0700

    farewell java, we are going to do something in python because DL4J sucks

--- install_external_libs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CURRENT_DIR=`pwd`
-mvn deploy:deploy-file -Dfile=./external_jars/deeplearning4j-core-0.4-rc3.9.jar -Durl="file://$CURRENT_DIR/repo/" -DgroupId=org.deeplearning4j -DartifactId=deeplearning4j-core -Dversion=0.4-rc3.9 -Dpackaging=jar
-mvn deploy:deploy-file -Dfile=./external_jars/lombok-1.16.7.jar -Durl="file://$CURRENT_DIR/repo/" -DgroupId=org.projectlombok -DartifactId=lombok -Dversion=1.16.7 -Dpackaging=jar
--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-git checkout master
-git pull
-git checkout PRODUCTION
-git merge master --no-edit
-git push origin PRODUCTION
