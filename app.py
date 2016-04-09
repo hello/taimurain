aca3ebb521d30b1ff5557521af1d2833737306be
Benjamin Joseph <benjo@sayhello.com>
2016-04-09 14:19:22 -0700

    work in progress

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-from flask import Flask
+from flask import Flask,url_for,request
 from ConfigParser import ConfigParser
 import logging
+from keras.models import model_from_json
+import neural_net_messages_pb2
 
 config_section_server = 'server'
 config_file_name = 'config.txt'
@@ -10,10 +12,30 @@ port_key = 'port'
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def list_routes():
+    import urllib
 
+    output = []
+    for rule in app.url_map.iter_rules():
+        methods = ','.join(rule.methods)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, rule))
+        output.append(line)
+
+    return sorted(output)
+
+@app.route('/')
+def status():
+    return '\n'.join(list_routes()) + '\n'
+
+@app.route('/v1/neuralnet/evaluate',methods=['POST'])
+def neural_net_v1():
+    m = neural_net_messages_pb2.NeuralNetInput()
+    m.ParseFromString(request.get_data())
+    print m.net_id
+
+    out = neural_net_messages_pb2.NeuralNetOutput()
+    
+    return out.SerializeToString(),200, {'Content-Type': 'application/octet-stream; charset=utf-8'}
 
 def main():
     config = ConfigParser()
