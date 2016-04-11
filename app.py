f0e517ce1124b63a9aa27ff8e7d4dd2123f679aa
Benjamin Joseph <benjo@sayhello.com>
2016-04-10 21:39:41 -0700

    using context instead of globals

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from flask import Flask,url_for,request
+from flask import Flask,url_for,request,g,current_app
 from ConfigParser import ConfigParser
 import logging
 from keras.models import model_from_json
@@ -9,6 +9,7 @@ import proto_utils
 import json
 import urllib
 import sys
+import thread
 
 config_section_server = 'server'
 config_file_name = sys.argv[1]
@@ -18,9 +19,6 @@ debug_key = 'debug'
 
 config_section_s3 = 's3'
 bucket_key = 'data-bucket'
-
-#yay global variables -- single threaded app, so no worries
-g_keras_models = {}
 
 app = Flask(__name__)
 
@@ -34,6 +32,7 @@ def list_routes():
 
     return sorted(output)
 
+
 @app.route('/')
 def status():
     routes = 'ROUTES:\n'.join(list_routes()) + '\n'
@@ -41,21 +40,32 @@ def status():
 
 @app.route('/v1/neuralnet/getavailable')
 def get_available_nets():
-    global g_keras_models
-    return json.dumps(g_keras_models.keys())
+    
+    keras_models = getattr(g, '_models', None)
+        
+    if keras_models == None:
+        logging.warn('endpoint=getavailable action=return_error reason=models_context_not_found' )
+        return '',400
+    
+    return json.dumps(keras_models.keys())
 
 @app.route('/v1/neuralnet/evaluate',methods=['POST'])
 def neural_net_v1():
-    global g_keras_models
 
+    keras_models = getattr(g, '_models', None)
+        
+    if keras_models == None:
+        logging.warn('endpoint=evaluate action=return_error reason=models_context_not_found' )
+        return '',400
+    
     m = neural_net_messages_pb2.NeuralNetInput()
     m.ParseFromString(request.get_data())
 
-    if m.net_id not in g_keras_models.keys():
-        logging.warn('action=return_error reason=net_not_found net_id=%s' % m.net_id)
+    if m.net_id not in keras_models.keys():
+        logging.warn('endpoint=evaluate action=return_error reason=net_not_found net_id=%s' % m.net_id)
         return '',400
 
-    model = g_keras_models[m.net_id]
+    model = keras_models[m.net_id]
 
     input_shape = model.inputs['input'].input_shape
 
@@ -70,9 +80,20 @@ def neural_net_v1():
     
     return out.SerializeToString(),200, {'Content-Type': 'application/octet-stream; charset=utf-8'}
 
-def main():
-    global g_keras_models
 
+@app.before_first_request
+def on_startup():
+    bucket = current_app.config['models_bucket']
+    
+    keras_models = getattr(g, '_models', None)
+
+    if keras_models == None:
+        g._models = models.get_models_from_s3(bucket)
+        logging.info('action=load_models_complete num_models=%d' % len(g._models))
+
+         
+    
+def main():
     config = ConfigParser()
 
     #read config file
@@ -91,11 +112,7 @@ def main():
         log_level = logging.DEBUG
         
     logging.basicConfig(level=log_level,format='%(levelname)s %(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-
-    
-    g_keras_models = models.get_models_from_s3(bucket)
-    logging.info('action=load_models_complete num_models=%d' % len(g_keras_models))
-
+    app.config['models_bucket'] = bucket
     app.run(host=host,port=port)
     
 
